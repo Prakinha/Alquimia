098c51262a4a719cc19e396cfbfc7990b4f681f7
FirstBits-WW <FirstBits90@gmail.com>
2026-05-24 19:19:37 -0300

    menu inicial alterado

--- menu/button.gd
+++ menu/button.gd
@@ -1,9 +1,13 @@
 extends Button
-
+var ButtonInicalTween:Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	ButtonInicalTween = create_tween()
+	ButtonInicalTween.set_loops(200)
+	ButtonInicalTween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
+	ButtonInicalTween.tween_property(self,"position:y", self.position.y + 14, 1.5 )
+	ButtonInicalTween.tween_property(self,"position:y", self.position.y - 6, 1.5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
