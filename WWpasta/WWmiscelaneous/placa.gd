c004cd10770e6ba3541ad0232460a58e4a6150ac
Prakinha <lolplaysgame@gmail.com>
2026-05-24 00:22:06 -0300

    Mudei dinamica das placas

--- WWpasta/WWmiscelaneous/placa.gd
+++ WWpasta/WWmiscelaneous/placa.gd
@@ -23,10 +23,12 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is PlayerCharacter:
-
 		if animacao_fade and animacao_fade.is_running():
 			animacao_fade.kill()
 			
 		animacao_fade = create_tween()
-		
 		animacao_fade.tween_property(texto, "modulate:a", 0.0, 0.3)
+
+		var maquina = get_parent()
+		if maquina.menu_aberto == true:
+			maquina.SubirOMenu()
