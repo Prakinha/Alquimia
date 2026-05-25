4b7c23c81273d2fd779ce38814f87ef574d04132
FirstBits-WW <FirstBits90@gmail.com>
2026-05-24 21:35:41 -0300

    aaaa

--- menu/Titulotweeners.gd
+++ menu/Titulotweeners.gd
@@ -1,11 +1,19 @@
 extends Control
+@onready var titulo: TextureRect = $Titulo
+@onready var aderecos: TextureRect = $Aderecos
+var titulotween:Tween
+var aderecotween:Tween
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	#titulotween = create_tween()
+	#titulotween.set_loops(200)
+	#titulotween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	#titulotween.tween_property(titulo,"position:y", titulo.position.y + 2, 3)
+	#titulotween.tween_property(titulo,"position:y", titulo.position.y - 2, 3)
+	#aderecotween = create_tween()
+	#aderecotween.set_loops(200)
+	#aderecotween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	#aderecotween.tween_property(aderecos,"position",aderecos.position + Vector2(16,2),3)
+	#aderecotween.tween_property(aderecos,"position",aderecos.position - Vector2(16,2),3)
 	pass
+	
